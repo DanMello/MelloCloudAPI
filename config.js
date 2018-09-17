93a3419cbcff723bbadf162c9ba6edbaa3729113
Dan Mello <jdanmello@gmail.com>
2018-09-17 03:11:58 -0400

    made changes to config for pm2

--- config.js
+++ config.js
@@ -57,7 +57,7 @@ exports = module.exports = function(app) {
           user: 'deploy',
           host: '10.0.0.201', // local ip or public ip if im not connect to local connection
           ref: 'origin/master',
-          repo: 'https://github.com/DanMello/mellocloud.com.git',
+          repo: 'https://github.com/DanMello/MelloCloudAPI.git',
           path: '/home/deploy/MelloCloud/MelloCloudAPI',
           'post-deploy' : 'nvm install && npm install && npm run build && /home/deploy/.nvm/versions/node/v8.11.3/bin/pm2 reload ecosystem.config.js --env production --only MelloCloudAPI'
         }
