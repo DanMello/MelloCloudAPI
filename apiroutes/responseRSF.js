6537fd07f5b4f59915045fa9e2c706d13f78d120
Dan Mello <jdanmello@gmail.com>
2019-05-27 18:54:29 -0400

    Updated version of RSF

--- apiroutes/responseRSF.js
+++ apiroutes/responseRSF.js
@@ -1,4 +1,4 @@
 exports.init = function (req, res) {
   let name = req.body.name || req.body['first_name'];
-  return res.send('Thank you ' + name + ' for trying react-simpler-forms v1.0.0!');
+  return res.send('Thank you ' + name + ' for trying react-simpler-forms v1.0.1!');
 };
